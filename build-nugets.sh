694c4961b349ec1b7a95b422520d142bc96bb25a
Albilaga Linggra Pradana <git@albilaga.id>
2024-12-05 13:16:10 +0700

    Include symbols

--- build-nugets.sh
+++ build-nugets.sh
@@ -3,7 +3,7 @@ projects=$(cat ./projects.txt)
 
 for project in ${projects}; do
 	cd ${project}
-	rm nugetoutput/*.nupkg
+	rm nugetoutput/*
 	dotnet clean -c Release
 	dotnet build -c Release
 	dotnet pack -c Release
